040eb38947d3c2eef7b23d88fae93662cf5049b9
Robin <56515210+Robin-mlh@users.noreply.github.com>
2022-08-20 16:40:42 +0200

    Mise à jour majeure
    - Multiples ajustements mineurs.
    - Multiples résolutions de bugs et amélioration de messages d'erreurs. 
    - Multiples optimisations et améliorations du code.

--- usi-lite.py
+++ usi-lite.py
@@ -20,9 +20,9 @@ if input("Mode client ou serveur ? [C/s] ") in ["s", "S", "serveur"]:
     serveur = True
 else:
     serveur = False
-    hote = input("Adresse du serveur (défaut 127.0.0.1) : ")
+    hote = input("Adresse du serveur à joindre (défaut localhost) : ")
     if not hote:
-        hote = "127.0.0.1"
+        hote = "localhost"
 try:
     port = input("Port (défaut 12800) : ")
     if not port:
@@ -45,13 +45,13 @@ def reception(connexion, infos_connexion):
                 ferme_connexion(infos_connexion, connexion)
             elif message_recu == "!serveur-fermeture" and not serveur:
                 ferme_connexion(infos_connexion, connexion)
-            elif message_recu.startswith("!!"):
-                afficher(message_recu[2:])
-            else:
+            elif "<système>" not in message_recu:
                 afficher(f"{infos_connexion[0]}>> {message_recu}")
                 if serveur:
                     envoi_message(message_recu, co_reception=connexion,
                                   afficher_message=False)
+            else:
+                afficher(message_recu)
         except (ConnectionResetError, ValueError, ConnectionAbortedError, OSError):
             ferme_connexion(infos_connexion, connexion)
             break
@@ -64,7 +64,7 @@ def thread_envoi():
     print("Appuyez sur ENTRÉE pour envoyer votre message."
           "\nEn attente de message entrant...")
     while not quitter:
-        msg_a_envoyer = input()
+        msg_a_envoyer = input().rstrip()
         if msg_a_envoyer == "":
             afficher()
         elif msg_a_envoyer == "!cls":
@@ -75,20 +75,18 @@ def thread_envoi():
             else:
                 afficher(f"Connexion établie avec le serveur: {hote}:{port}\n")
         elif msg_a_envoyer in ["!help", "!aide", "!aled"]:
-            afficher(f"""{msg_a_envoyer} >\n   !cls      Nettoyer l'écran
-   !fermer   Fermer la connexion""")
-        else:
-            envoi_message(msg_a_envoyer)
-        if msg_a_envoyer in ["!fermer", "!ferme"]:
+            afficher(f"""{msg_a_envoyer} >\n   !cls      Nettoyer l'écran\n   !fermer   Fermer la connexion""")
+        elif msg_a_envoyer == "!fermer":
             if serveur:
                 envoi_message("!serveur-fermeture", afficher_message=False)
             else:
                 envoi_message("!client-fermeture", afficher_message=False)
             ferme_connexion(infos_connexion)
+        else:
+            envoi_message(msg_a_envoyer)
 
 
-def envoi_message(message, co_reception=None,
-                  afficher_message=True):
+def envoi_message(message, co_reception=None, afficher_message=True):
     if afficher_message:
         afficher(message)
     for co in liste_connexions:
@@ -100,24 +98,26 @@ def ferme_connexion(infos_connexion, connexion=None):
     global liste_connexions
     global quitter
 
-    if not connexion or not serveur:
-        if not quitter:
+    if not quitter:
+        if not connexion or not serveur:
             quitter = True
             for co in liste_connexions:
                 co.shutdown(socket.SHUT_WR)
             liste_connexions = []
-        afficher("<Système> Connexion fermée.")
-        exit(0)
-    elif not quitter:
-        connexion.close()
-        try:
-            liste_connexions.remove(connexion)
-            afficher(f"<Système> Le client {infos_connexion[0]} s'est déconnecté.")
-            if liste_connexions:
-                envoi_message(f"!!<Système> Le client {infos_connexion[0]} s'est déconnecté.",
-                              co_reception=connexion, afficher_message=False)
-        except Exception:
-            pass
+            afficher("<système> Connexion fermée.")
+            exit(0)
+        else:
+            connexion.close()
+            try:
+                liste_connexions.remove(connexion)
+                afficher(f"<système> Le client {infos_connexion[0]} s'est déconnecté.")
+                if liste_connexions:
+                    envoi_message(f"<système> Le client {infos_connexion[0]} s'est déconnecté.",
+                                  co_reception=connexion, afficher_message=False)
+                if not liste_connexions:
+                    print("\nEn attente de client...  ctrl+c pour quitter")
+            except Exception:
+                pass
 
 
 def afficher(nouvel_element=None):
@@ -135,7 +135,7 @@ if serveur:
     try:
         sock.bind(("", port))
     except OSError:
-        raise SystemExit("Erreur: ce port est déjà utilisé.")
+        raise SystemExit(f"Le port {port} est déjà utilisé.")
     try:
         sock_ip_locale = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock_ip_locale.connect(('10.255.255.255', 1))
@@ -148,27 +148,24 @@ if serveur:
     sock.listen()
     sock.settimeout(0.5)
     thread_envoi_lance = False
-    message_attente = True
+    if not liste_connexions:
+        print("En attente de client...  ctrl+c pour quitter")
     while not quitter:
         while not quitter:
-            if len(liste_connexions) == 0 and message_attente:
-                print("\nEn attente de client...  Ctrl+C pour quitter")
-                message_attente = False
             try:
                 connexion, infos_connexion = sock.accept()
-                message_attente = True
                 break
             except (socket.timeout, OSError):
                 pass
         if quitter:
             break
         liste_connexions.append(connexion)
-        afficher(f"<Système> Le client {infos_connexion[0]} c'est connecté.")
+        afficher(f"<système> Le client {infos_connexion[0]} c'est connecté.")
         threading.Thread(target=reception, args=[connexion, infos_connexion]).start()
         if not thread_envoi_lance:
             threading.Thread(target=thread_envoi).start()
             thread_envoi_lance = True
-        envoi_message(f"!!<Système> Le client {infos_connexion[0]} c'est connecté.",
+        envoi_message(f"<système> Le client {infos_connexion[0]} c'est connecté.",
                       co_reception=connexion, afficher_message=False)
 else:
     infos_connexion = (hote, port)
@@ -180,13 +177,13 @@ else:
             break
         except (ConnectionRefusedError, TimeoutError) as e:
             if afficher_en_attente:
-                print(f"En attente du serveur ({hote}:{port})...   (Ctrl+C pour annuler)")
+                print(f"En attente du serveur ({hote}:{port})...   ctrl+c pour quitter")
                 afficher_en_attente = False
             time.sleep(0.2)
         except (socket.gaierror, OSError):
-            raise SystemExit("Erreur: adresse invalide.")
+            raise SystemExit(f"L'adresse {hote} est invalide.")
         except OverflowError:
-            raise SystemExit("Erreur: port invalide.")
+            raise SystemExit(f"Le port {port} invalide.")
     afficher(f"Connexion établie avec le serveur {hote}:{port}\n")
     liste_connexions.append(connexion)
     threading.Thread(target=reception, args=[connexion, (hote, port)]).start()
