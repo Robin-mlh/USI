122e61fe9b25df642d14dbc636a97230287ac560
Robin <56515210+Robin-mlh@users.noreply.github.com>
2022-08-28 20:43:09 +0200

    Correction de bugs majeurs et ajustements. USI 2.2
    - Amélioration du système qui donne l'ip locale.
    - Résolution d'un bug majeur : les adresses (ip) affichées étaient incorrectes.
    - Multiples modifications mineures.

--- usi.py
+++ usi.py
@@ -26,7 +26,7 @@ from Cryptodome import Random
 LEN_BUFFER_TCP = 16384  # Taille du tampon socket en bits.
 CLE_RSA = 2048  # Taille de la clé RSA en bits.
 CLE_SESSION = 16  # Taille de la clé de session symétrique AES en octets.
-USI_VERSION = "USI 2.1"
+USI_VERSION = "USI 2.2"
 
 liste_ecran = []  # Liste des éléments à afficher.
 liste_cle_session = []  # Liste des clés de session.
@@ -64,17 +64,19 @@ def reception(connexion, infos_connexion, cle):
                                 message_client="<système> Le serveur a fermé la connexion. ENTRÉE pour fermer.")
             elif message_recu == "!client-utilisateurs" and arguments.serveur:
                 # Réponse du serveur pour la demande d'un client de la liste des utilisateurs.
-                liste_utilisateurs = f"!utilisateurs >\n   Serveur: (ip_serveur) (empreinte_serveur)\n   Clients:"
+                liste_utilisateurs = ""
                 for co in liste_connexions:
                     if co == connexion:
-                        liste_utilisateurs += f"\n            {co.getsockname()[0]}  (Vous)"
+                        liste_utilisateurs += f"\n            {co.getpeername()[0]}  (Vous)"
                     else:
-                        liste_utilisateurs += f"\n            {co.getsockname()[0]}  ({empreintes_verif[co]})"
+                        liste_utilisateurs += f"\n            {co.getpeername()[0]}  ({empreintes_verif[co]})"
+                liste_utilisateurs = ''.join(("!utilisateurs >\n   Serveur: (ip_serveur) (empreinte_serveur)"
+                                              "\n   Clients:", liste_utilisateurs[12:]))
                 envoi_message(liste_utilisateurs, co_unique=connexion, afficher_message=False)
             elif message_recu.startswith("!utilisateurs") and not arguments.serveur:
                 # Traitement de la réponse du serveur contenant la liste des utilisateurs.
                 liste_utilisateurs = message_recu.replace('(empreinte_serveur)', f'({empreintes_verif[connexion]})')
-                afficher(f"{liste_utilisateurs.replace('(ip_serveur)', connexion.getsockname()[0])}")
+                afficher(f"{liste_utilisateurs.replace('(ip_serveur)', hote)}")
             elif message_recu.startswith("!:f"):  # Reception d'un fichier.
                 fichier, taille_fichier = message_recu[3:].split("|")
                 taille_fichier = int(taille_fichier)
@@ -100,13 +102,14 @@ def reception(connexion, infos_connexion, cle):
                     citation = f"               \>{message_temp[1]}"
                     message_temp = ">>".join(message_temp[2:])
                     afficher(f"{infos_connexion[0]}>> {message_temp}\n{citation}")
-                elif not arguments.dev and "<système>" not in message_recu:  # Message normal.
-                    afficher(f"{infos_connexion[0]}>> {message_recu}")
-                elif not arguments.dev:  # Message système.
+                elif "<système>" in message_recu and not arguments.dev:  # Message système.
                     afficher(message_recu)
-                if arguments.serveur:
-                    # Si serveur: relai aux autres clients du message (rechiffré par le serveur).
-                    envoi_message(message_recu, co_reception=connexion,
+                elif message_recu.startswith("!:s") and not arguments.dev:  # Client: Message normal.
+                    afficher(message_recu[3:])
+                elif not arguments.dev:  # Serveur: Message normal.
+                    afficher(f"{infos_connexion[0]}>> {message_recu}")
+                if arguments.serveur and len(liste_connexions) > 1:  # relai aux autres clients du message.
+                    envoi_message(f"!:s{infos_connexion[0]}>> {message_recu}", co_reception=connexion,
                                   afficher_message=False)
         except (ConnectionResetError, ValueError, ConnectionAbortedError, OSError):
             ferme_connexion(infos_connexion, connexion, message_client="<système> Erreur: connexion terminée.")
@@ -123,6 +126,8 @@ def thread_envoi():
     print("Appuyez sur ENTRÉE pour envoyer votre message.\nEn attente de message entrant...")
     while not quitter:
         msg_a_envoyer = input().rstrip()  # Demande le message à envoyer (ou la commande à executer).
+        if msg_a_envoyer.startswith("!:s"):
+            msg_a_envoyer = msg_a_envoyer[3:]
         if msg_a_envoyer == "":  # Message vide: met à jour l'écran sans nouveau message.
             afficher(notification=False)
         elif msg_a_envoyer == "!cls":  # Supprimer les messages affichés.
@@ -150,11 +155,11 @@ def thread_envoi():
             if not arguments.serveur:  # Demande la liste au serveur.
                 envoi_message("!client-utilisateurs", afficher_message=False)
             else:  # Affiche la liste.
-                liste_utilisateurs = (f"!utilisateurs >\n   Serveur: {sock.getsockname()[0]}  (Vous)"
-                                      f"\n   Clients: {liste_connexions[0].getsockname()[0]}  "
-                                      f"({empreintes_verif[liste_connexions[0]]})")
-                for co in liste_connexions[1:]:
-                    liste_utilisateurs += f"\n            {co.getsockname()[0]}  ({empreintes_verif[co]})"
+                liste_utilisateurs = ""
+                for co in liste_connexions:
+                    liste_utilisateurs += f"\n            {co.getpeername()[0]}  ({empreintes_verif[co]})"
+                liste_utilisateurs = ''.join((f"!utilisateurs >\n   Serveur: (Vous)\n   Clients:",
+                                              liste_utilisateurs[12:]))
                 afficher(liste_utilisateurs)
         elif msg_a_envoyer in ["!help", "!aide", "!aled"]:  # Message d'aide des commandes.
             afficher(f"{msg_a_envoyer} >\n   Pour citer un message: '>>citation>>message'"
@@ -187,7 +192,7 @@ def envoi_message(message, co_reception=None, co_unique=None, afficher_message=T
                                f"{base64.b64encode(texte_chiffre).decode()}")
             co.sendall(message_chiffre.encode())  # Envoi du message chiffré.
             if arguments.dev:  # Mode développeur.
-                afficher(f"[dev] {message} ({message_chiffre}) >> {co.getsockname()}", notification=False)
+                afficher(f"[dev] {message} ({message_chiffre}) >> {co.getpeername()}", notification=False)
 
 
 def envoi_fichier(fichier, co_reception=None, afficher_message=True):
@@ -221,8 +226,7 @@ def ferme_connexion(infos_connexion=None, connexion=None, message_client=None):
     global quitter
 
     if not quitter:
-        if not connexion or not arguments.serveur:
-            # Déconnexion totale et fermeture du programme.
+        if not connexion or not arguments.serveur:  # Déconnexion totale et fermeture du programme.
             quitter = True
             if message_annulation:
                 print("\n<système> Annulation.")
@@ -255,25 +259,17 @@ def afficher(nouvel_element=None, notification=True):
 
     if nouvel_element is not None:  # Ajout du nouvel élément à la liste affichée.
         liste_ecran.append(nouvel_element)
-        if notification:
-            envoi_notification(nouvel_element)
+        if notification and not arguments.notifications:  # Notification.
+            try:
+                if arguments.serveur:
+                    plyer.notification.notify("USI serveur", contenu)
+                else:
+                    plyer.notification.notify("USI client", contenu)
+            except Exception:
+                pass
     os.system(cls)  # Nettoyage de l'écran.
     for element in liste_ecran:  # Affiche chaque élément.
         print(element)
-
-
-def envoi_notification(contenu):
-    """ Déclenche une notification système. """
-
-    if not arguments.notifications:
-        try:
-            if arguments.serveur:
-                plyer.notification.notify("USI serveur", contenu)
-            else:
-                plyer.notification.notify("USI client", contenu)
-        except Exception:
-            # Impossible d'envoyer une notification.
-            pass
 
 
 def signal_handler(signal, frame):
@@ -289,9 +285,9 @@ def signal_handler(signal, frame):
 
 signal(SIGINT, signal_handler)  # Gestion du ctrl+c.
 # Définition des arguments de la commande.
-argumentParser = argparse.ArgumentParser(description="Communications simples et chiffrées."
-                                                     "\nUne fois connecté, utilisez la commande '!aide' "
-                                                     "pour afficher les commandes disponibles.",
+argumentParser = argparse.ArgumentParser(description="USI: Communications simples et chiffrées."
+                                                     " github.com/Robin-mlh/USI\nUne fois connecté, utilisez"
+                                                     " la commande '!aide' pour afficher les commandes disponibles.",
                                          usage="USI.py [-h] {-s | -c [-i HOTE]} [-p PORT] [-nd]",
                                          formatter_class=argparse.RawTextHelpFormatter, add_help=False)
 argumentParser._optionals.title = 'Options'
@@ -328,24 +324,26 @@ if arguments.serveur:  # Serveur.
     try:
         sock.bind(("", arguments.port))
     except OSError:
-        raise SystemExit(f"Le port '{arguments.port}' est déjà utilisé.")
+        raise SystemExit(f"Le port {arguments.port} est déjà utilisé.")
     try:  # Obtention de l'ip publique.
-        ip_publique = f"Adresse ip publique: {json.loads(request.urlopen('http://httpbin.org/ip').read())['origin']}"
+        ip_publique = f"Adresse ip publique: {json.loads(request.urlopen('https://httpbin.org/ip').read())['origin']}"
     except error.URLError:  # Adresse ip publique non disponible, vérifier la connexion.
         ip_publique = ""
     try:  # Obtention de l'ip locale.
         sock_ip_locale = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock_ip_locale.connect(('10.255.255.255', 1))
-        ip_locale = sock.getsockname()[0]
-    except Exception:
-        ip_locale = "127.0.0.1"
+        sock_ip_locale.connect(('1.1.1.1', 80))
+        ip_locale = sock_ip_locale.getsockname()[0]
+    except socket.error:
+        try:
+            ip_locale = socket.gethostbyname(socket.gethostname())
+        except socket.gaierror:
+            ip_locale = "127.0.0.1"
     sock_ip_locale.close()
     afficher(f"Adresse ip locale: {ip_locale}   {ip_publique}   Port écouté: {arguments.port}\n", notification=False)
     sock.listen()
     sock.settimeout(0.5)
     thread_envoi_lance = False
-    if not liste_connexions:
-        print("En attente de client...  ctrl+c pour quitter")
+    print("En attente de client...  ctrl+c pour quitter")
     while not quitter:
         while not quitter:
             try:
@@ -393,6 +391,7 @@ if arguments.serveur:  # Serveur.
 else:  # Client.
     infos_connexion = (hote, arguments.port)
     connexion = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print(f"Connexion à {hote}:{arguments.port}...   ctrl+c pour annuler")
     afficher_en_attente = True
     while True:  # Jusqu'à qu'une connexion soit créée.
         try:
@@ -400,15 +399,16 @@ else:  # Client.
             break
         except (ConnectionRefusedError, TimeoutError) as e:
             if afficher_en_attente:
-                print(f"En attente du serveur ({hote}:{arguments.port})...   ctrl+c pour quitter")
+                afficher(f"En attente du serveur ({hote}:{arguments.port})...   ctrl+c pour quitter",
+                         notification=False)
                 afficher_en_attente = False
             time.sleep(0.2)
         except (socket.gaierror, OSError):
-            raise SystemExit(f"Adresse '{hote}' invalide.")
+            raise SystemExit(f"Adresse {hote} invalide.")
         except OverflowError:
-            raise SystemExit(f"Port '{arguments.port}' invalide.")
+            raise SystemExit(f"Port {arguments.port} invalide.")
     message_annulation = False
-    print("Échange sécurisé de la clé de session...")
+    afficher("Échange sécurisé de la clé de session...", notification=False)
     cle_privee = RSA.generate(CLE_RSA)  # Génération de la clé RSA.
     cle_publique = cle_privee.public_key()  # Génération de la clé publique avec la clé privée.
     cle_session = Random.get_random_bytes(CLE_SESSION)  # Génération de la clé de session.
@@ -434,6 +434,7 @@ else:  # Client.
     liste_cle_session.append(cle_session)  # Ajout de la clé de session à la liste des clés.
     empreintes_verif[connexion] = SHA256.new(cle_publique.export_key("DER") +
                                              cle_publique_serveur.export_key("DER")).hexdigest()
+    liste_ecran = []  # Réinitialise l'écran.
     afficher(f"Connexion sécurisée établie avec le serveur {hote}:{arguments.port}\n"
              f"Empreinte de vérification: {empreintes_verif[connexion]}\n", notification=False)
     # Lance l'unique thread de réception des messages du serveur.
